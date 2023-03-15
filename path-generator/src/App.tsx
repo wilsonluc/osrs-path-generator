2bf27a024ccb03e4fa3c132a30c435911c595b0d
wilsonluc <wilsonluc09@gmail.com>
2023-03-15 20:15:03 +1100

    Leaflet skeleton

--- path-generator/src/App.tsx
+++ path-generator/src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<MapContainer center={[-37.808, 144.962]} zoom={20} scrollWheelZoom={true}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={[-37.808, 144.962]}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
   );
 }
 
